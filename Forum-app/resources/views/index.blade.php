a938301306070d5b8d1ee308bffc47719fd316ef
saadchaay <chaaysaad@gmail.com>
2022-04-12 22:33:15 +0000

    add post page index

--- Forum-app/resources/views/index.blade.php
+++ Forum-app/resources/views/index.blade.php
@@ -1,1 +1,22 @@
-Home
+@extends('layouts.app')
+
+@section('content')
+
+    <main class="sm:container sm:mx-auto sm:mt-10">
+        <div class="flex justify-around">
+                <div class="max-w-sm bg-white border-2 border-gray-300 p-6 rounded-md tracking-wide shadow-lg">
+                    <div id="header" class="flex items-center mb-4">
+                        <img alt="avatar" class="w-20 rounded-full border-2 border-gray-300" src="https://picsum.photos/seed/picsum/200" />
+                        <div id="header-text" class="leading-5 ml-6 sm">
+                            <h4 id="name" class="text-xl font-semibold">John Doe</h4>
+                            <h5 id="job" class="font-semibold text-blue-600">Designer</h5>
+                        </div>
+                    </div>
+                    <div id="quote">
+                        <q class="italic text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</q>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
