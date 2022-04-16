bb430608d6d1329b62bfc771c93f26e817c59b17
saadchaay <chaaysaad@gmail.com>
2022-04-16 01:21:23 +0000

    modifier le fichier home controller

--- Forum-app/app/Http/Controllers/HomeController.php
+++ Forum-app/app/Http/Controllers/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
      *
      * @return Response
      */
-    public function index(): Response
+    public function index()
     {
         return view('home');
     }
